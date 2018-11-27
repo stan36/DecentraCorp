7c04c27f6abae206e5424aeeaba8b66c390ba78d
cryptogrow36 <cryptogrow36@gmail.com>
2018-11-26 16:43:00 -0800

    realized react router doesnt work properly with outside links, fixed using href instead

--- front/src/views/About.js
+++ front/src/views/About.js
@@ -27,21 +27,21 @@ class About extends Component {
         <br/>
         <p>
         To view the DecentraCorp WhitePaper
-        <button><Link style={{ textDecoration: 'none', color: "red"}} to='//drive.google.com/open?id=1hhsuQ_JnBz5E0813V-4trPZhiflaTBwr'> Click Here!</Link></button>
+        <a style={{ textDecoration: 'none', color: "red"}} href='https://drive.google.com/open?id=1hhsuQ_JnBz5E0813V-4trPZhiflaTBwr'> Click Here!</a>
         </p>
         </div>
         <br/>
         <div>
         <p>
         To view the WhitePaper for the CryptoPatent Blockchain
-        <button><Link style={{ textDecoration: 'none', color: "red"}} to='//drive.google.com/open?id=1Me8pn45kzBCucV-F1LyjYHv4LLBKWX6w'> Click Here!</Link></button>
+        <a style={{ textDecoration: 'none', color: "red"}} href='https://drive.google.com/open?id=1Me8pn45kzBCucV-F1LyjYHv4LLBKWX6w'> Click Here!</a>
         </p>
         </div>
         <br/>
         <div>
         <p>
         To view the ChaosCasino's WhitePaper
-        <button><Link style={{ textDecoration: 'none', color: "red"}} to='//drive.google.com/open?id=12Fw9pM8V9B1zs1QvsQpNDL504ylq84Ic'> Click Here!</Link></button>
+        <a style={{ textDecoration: 'none', color: "red"}} href='https://drive.google.com/open?id=12Fw9pM8V9B1zs1QvsQpNDL504ylq84Ic'> Click Here!</a>
         </p>
         </div>
       </div>
